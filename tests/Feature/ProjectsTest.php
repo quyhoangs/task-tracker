b1426202fe6c60a5eb1b726a418fca7c8e39356c
quyhoangs <quyromutc@gmail.com>
2022-10-08 16:11:07 +0700

    3 Testing Request Validation

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Project;
 
 class ProjectsTest extends TestCase
 {
@@ -13,7 +14,8 @@ class ProjectsTest extends TestCase
     /** @test */
     public function test_a_user_can_create_a_project()
     {
-        $this->withoutExceptionHandling();
+        $this->withoutExceptionHandling(); //không Xử lý Ngoại lệ để test
+
         $attributes=[
             'title'=>$this->faker->sentence,
             'description'=>$this->faker->paragraph
@@ -26,5 +28,23 @@ class ProjectsTest extends TestCase
 
         // //Khẳng định có thể gửi 1 yêu cầu get và thấy các thuộc tính đã tạo trên
         $this->get('/projects')->assertSee($attributes['title']);
+    }
+
+    /** @test */
+    public function test_a_project_requires_a_title()
+    {
+        //Tạo các thuộc tính với factory, field title rỗng và không lưu vào db
+        $attributes= Project::factory()->raw(['title'=>'']); //raw() trả về mảng || make() trả về 1 object || create() trả về object và save db
+
+        //Gửi 1 yêu cầu post để lưu vào db, trường hợp nếu title chưa validate ở Controller sẻ báo lỗi
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function test_a_project_requires_a_description()
+    {
+        $attributes=Project::factory()->raw(['description'=>'']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
 }
