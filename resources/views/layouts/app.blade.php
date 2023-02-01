6a3b60abb8a2e66c8c8d181a19bbdbf9ff53bf4a
QuyHoangMor <quyhx@mor.com.vn>
2023-02-01 23:05:56 +0700

    Screen Projects UI - with Tailwin and str_limit charactor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,17 +21,11 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <nav class="bg-white">
+            <div class="container mx-auto">
+                <div class="flex justify-between">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
+                    <a href=""> BIRBOARD </a>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
@@ -72,7 +66,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="py-4 container mx-auto" >
             @yield('content')
         </main>
     </div>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts.app')
 @section('content')
-    <h1 class="text-3xl font-bold underline">Task Managermant</h1>
-    <ul >
-        @forelse($projects as $project)
-            <li>
-                <a href="{{$project->path()}}">{{ $project->title }}</a>
-            </li>
+<div class="bg-gray-100"  style="height:800px">
+    <div class="flex items-center mb-3">
+        <a href="/project/create">New Project</a>
+    </div>
+
+    <div class="flex ">
+        @forelse ($projects as $project)
+        <div class="bg-white mr-4 rounded shadow w-1/3 p-5" style="height:200px">
+            <h3 class="font-normal text-xl py-4"> {{ $project->title }} </h3>
+            <div class="text-gray-700"> {{ \Illuminate\Support\Str::limit( $project->description ,100 ) }} </div>
+        </div>
         @empty
-            <li>Nothing show</li>
+        <p>No projects yet.</p>
         @endforelse
-    </ul>
+    </div>
+</div>
 @endsection
