a5abe8c8f409dd71d2dfa753d8392cbe4326b195
QuyHoangMor <quyhx@mor.com.vn>
2023-02-18 23:45:25 +0700

    Rendering Activity of project to view - multile view render

---
+++ resources/views/projects/activity/card.blade.php
@@ -0,0 +1,16 @@
+<div class="card mt-3">
+    <ul class="text-xs list-reset">
+
+        @foreach ($project->activity as $activity)
+            <li class="{{ $loop->last ? '' : 'mb-1' }}">
+                @if(View::exists("projects.activity.{$activity->description}"))
+                    @include ("projects.activity.{$activity->description}")
+                @else
+                    {{ $activity->description }}
+                @endif
+                <span class="text-grey">{{ $activity->created_at->diffForHumans(null,null,true) }}</span>
+            </li>
+        @endforeach
+
+    </ul>
+</div>
