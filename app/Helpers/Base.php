aee54f2de52e115f598512a66f0875a1a803bb9e
QuyHoangMor <quyhx@mor.com.vn>
2023-02-27 20:29:08 +0700

    Add avatar of owner and member in project

--- app/Helpers/Base.php
+++ app/Helpers/Base.php
@@ -19,3 +19,16 @@ function _tr($key = null, $replace = [], $locale = null)
     return ($key === ($translation = trans($key, $replace, $locale))) ? '' : $translation;
 }
 
+
+// Generates a gravatar URL for the given email address
+// $email: the email address to generate a gravatar URL for
+function gravatar_url($email)
+{
+    $email = md5($email);
+
+    return "https://gravatar.com/avatar/{$email}?" . http_build_query([
+        's' => 60,
+        'd' => 'https://s3.amazonaws.com/laracasts/images/default-square-avatar.jpg'
+    ]);
+    // example: https://gravatar.com/avatar/quyhoang@gmail.com?s=60&d=https://s3.amazonaws.com/laracasts/images/default-square-avatar.jpg
+}
